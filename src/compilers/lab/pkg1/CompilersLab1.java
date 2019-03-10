84da2306ab4ea59b9ddcc033cfb686afaed2bbec
AhmedAlSai <16P6000@eng.asu.edu.eg>
2019-03-10 17:11:27 +0200

    Finish functionalities without testing

--- src/compilers/lab/pkg1/CompilersLab1.java
+++ src/compilers/lab/pkg1/CompilersLab1.java
@@ -20,6 +20,7 @@ public class CompilersLab1 {
 
     public static ArrayList<String> reserved = new ArrayList<String>();
     public static ArrayList<String> singleCharSpecial = new ArrayList<String>();
+    public static ArrayList<String> multipleCharSpecialsStartChar = new ArrayList<String>();
     public static int CurrentCharASCII;
 
     /**
@@ -40,31 +41,20 @@ public class CompilersLab1 {
                 } else if (singleCharSpecial.contains((char) CurrentCharASCII + "")) {
                     System.out.println("Special symbol: " + (char) CurrentCharASCII);
                     readNext(fr);
-                } else if (!isDigit(currentChar)) {
-                    String strIfExists = "";
-                    while (true) {
-                        String tempStr = seeIfReserved(currentChar, fr);
-                        System.out.println(tempStr);
-                        if (!tempStr.equals("") && !Character.isLetter(tempStr.charAt(0))) {
-                            //Add identifier token
-                            if (strIfExists.length() != 0) {
-                                System.out.println("Identifier: " + strIfExists);
-                            }
-                            //Call ,multiple character symbol function
-                            seeIfMultipleCharSymbol(tempStr);
-                            break;
-                        } else if (!tempStr.equals("") && Character.isLetter(tempStr.charAt(0))) {
-                            //keep going because is is a letter
-                            strIfExists += tempStr;
-                            continue;
-                        } else {
-                            break;
-                        }
-                    }
-                } else if (isDigit(currentChar)) {
-                    seeIfvalidDigit(currentChar, fr);
                 }
-
+                else if (multipleCharSpecialsStartChar.contains((char) CurrentCharASCII + "")){
+                    seeIfMultipleCharSpecial((char)CurrentCharASCII, fr);
+                }
+                else if (Character.isLetter((char)CurrentCharASCII)){
+                    seeIfReservedOrIdentifier(fr);
+                }
+                else if (Character.isDigit((char)CurrentCharASCII)){
+                    getNumber(fr);
+                }
+                
+                else {
+                    readNext(fr);
+                }
             }
         } catch (Exception ex) {
             System.out.println(ex);
@@ -97,138 +87,7 @@ public class CompilersLab1 {
 
         }
     }
-
-    public static String seeIfReserved(char x, FileReader fr) throws IOException {
-        String tempStr = "";
-        if (x == 'i') {
-            tempStr += "i";
-            char y = (char) readNext(fr);
-            if (y == 'f') {
-                System.out.println("Reserved word: if");
-                readNext(fr);
-                return "";
-            }
-        } else if (x == 'r') {
-            tempStr += "r";
-            if ((char) readNext(fr) == 'e') {
-                tempStr += "e";
-                char temp = (char) readNext(fr);
-                if (temp == 'a') {
-                    tempStr += "a";
-                    if ((char) readNext(fr) == 'd') {
-                        System.out.println("Reserved word: read");
-                        readNext(fr);
-                        return "";
-                    }
-                } else if (temp == 'p') {
-                    tempStr += "p";
-                    if ((char) readNext(fr) == 'e') {
-                        tempStr += "e";
-                        if ((char) readNext(fr) == 'a') {
-                            tempStr += "a";
-                            if ((char) readNext(fr) == 't') {
-                                System.out.println("Reserved word: repeat");
-                                readNext(fr);
-                                return "";
-                            }
-                        }
-                    }
-                }
-            }
-        } else if (x == 'e') {
-            tempStr += "e";
-            char temp = (char) readNext(fr);
-            if (temp == 'l') {
-                tempStr += "l";
-                if ((char) readNext(fr) == 's') {
-                    tempStr += "s";
-                    if ((char) readNext(fr) == 'e') {
-                        System.out.println("Reserved word: else");
-                        readNext(fr);
-                        return "";
-                    }
-                }
-            } else if (temp == 'n') {
-                tempStr += "n";
-                if ((char) readNext(fr) == 'd') {
-                    System.out.println("Reserved word: end");
-                    readNext(fr);
-                    return "";
-                }
-            }
-        } else if (x == 't') {
-            tempStr += "t";
-            if ((char) readNext(fr) == 'h') {
-                tempStr += "h";
-                if ((char) readNext(fr) == 'e') {
-                    tempStr += "e";
-                    if ((char) readNext(fr) == 'n') {
-                        System.out.println("Reserved word: then");
-                        readNext(fr);
-                        return "";
-                    }
-                }
-            }
-        } else if (x == 'u') {
-            tempStr += "u";
-            if ((char) readNext(fr) == 'n') {
-                tempStr += "n";
-                if ((char) readNext(fr) == 't') {
-                    tempStr += "t";
-                    if ((char) readNext(fr) == 'i') {
-                        tempStr += "i";
-                        if ((char) readNext(fr) == 'l') {
-
-                            System.out.println("Reserved word: until");
-                            readNext(fr);
-                            return "";
-                        }
-                    }
-                }
-            }
-        } else if (x == 'w') {
-            tempStr += "w";
-            if ((char) readNext(fr) == 'r') {
-                tempStr += "r";
-                if ((char) readNext(fr) == 'i') {
-                    tempStr += "i";
-                    if ((char) readNext(fr) == 't') {
-                        tempStr += "t";
-                        if ((char) readNext(fr) == 'e') {
-
-                            System.out.println("Reserved word: write");
-                            readNext(fr);
-                            return "";
-
-                        }
-                    }
-                }
-            }
-        } else {
-            System.out.println("WILL read next");
-            readNext(fr);
-            System.out.println("Current char: " + (char) CurrentCharASCII);
-            return x + "";
-        }
-        return tempStr;
-
-    }
-
-    public static void seeIfvalidDigit(char x, FileReader fr) throws IOException {
-
-        char temp;
-        String number = x + "";
-        while (true) {
-            temp = (char) readNext(fr);
-            if (isDigit(temp)) {
-                number += temp;
-            } else {
-                break;
-            }
-        }
-        System.out.println("Number: " + number);
-    }
-
+    
     private static void init() {
         reserved.add("read");
         reserved.add("if");
@@ -244,9 +103,9 @@ public class CompilersLab1 {
         singleCharSpecial.add("/");
         singleCharSpecial.add("(");
         singleCharSpecial.add(")");
-        singleCharSpecial.add(":=");
         singleCharSpecial.add(";");
         singleCharSpecial.add("<");
+        multipleCharSpecialsStartChar.add(":");
 
     }
 
@@ -255,6 +114,36 @@ public class CompilersLab1 {
         return CurrentCharASCII;
     }
 
-    private static void seeIfMultipleCharSymbol(String tempStr) {
+    private static void seeIfMultipleCharSpecial(char c, FileReader fr) throws IOException {
+        if (c == ':'){
+            readNext(fr);
+            if ((char)CurrentCharASCII == '='){
+                System.out.println("Special symbol: :=");
+                readNext(fr);
+            }
+        }
+    }
+
+    private static void seeIfReservedOrIdentifier(FileReader fr) throws IOException {
+        String tempStr = (char) CurrentCharASCII + "";
+        char tempChar;
+        while (Character.isLetter(tempChar = (char) readNext(fr))) {
+            tempStr += tempChar;
+        }
+        if (reserved.contains(tempStr)) {
+            System.out.println("Reserved symbol: " + tempStr);
+        } else {
+            System.out.println("Identifier: " + tempStr);
+
+        }
+    }
+    
+    private static void getNumber(FileReader fr) throws IOException{
+        String tempStr = (char) CurrentCharASCII + "";
+        char tempChar;
+        while (Character.isDigit(tempChar = (char) readNext(fr))) {
+            tempStr += tempChar;
+        }
+        System.out.println("Number: " + tempStr);
     }
 }
